c8e72f6ee768a87a123ddc76f7f77552fcfff025
hebamesluh1 <hebamesluh@gmail.com>
2023-08-28 14:46:15 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { GlobalStyle } from "./global/style";
 import Spinner from "./components/Spinner";
 import Home from "./pages/Home";
 import SideMenu from "./components/SideMenu";
+import Header from "./components/Header";
 
 export default function App() {
   const [theme, setTheme] = useState(darkTheme);
@@ -17,13 +18,24 @@ export default function App() {
     console.log(theme);
   }, [theme]);
 
+  const [close, setClose] = useState(false);
+
+  const closeClick = () => {
+    setClose(false);
+  }
+  const openClick = () => {
+    setClose(true);
+  }
+  console.log(close)
+
   return (
     <ThemeProvider theme={theme}>
       <themeContext.Provider value={[theme, setTheme]}>
         <GlobalStyle />
         <Suspense fallback={<Spinner/>}>
-          {/* <Home/> */}
-          <SideMenu/>
+          <Header openClick={openClick}/>
+          <Home/>
+          <SideMenu close={close} closeClick={closeClick}/>
         </Suspense>
       </themeContext.Provider>
     </ThemeProvider>
