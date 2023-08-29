b167e2ad1364a139f34a91311ff33e69df08d472
hebamesluh1 <hebamesluh@gmail.com>
2023-08-29 17:18:41 +0300

    .

--- src/pages/Home/US/style.js
+++ src/pages/Home/US/style.js
@@ -1,45 +1,45 @@
 import styled from "styled-components";
 
 export const USStyle = styled.div`
-    padding:10% 0;
+  padding: 10% 0;
   .us-content {
-    position:relative;
+    position: relative;
     box-shadow: 0px 4px 50px 0px #1f81b01a;
     width: 95%;
     border-radius: 16px;
     padding: 10px 20px;
-    margin:10% auto;
-    z-index:1000;
+    margin: 10% auto;
+    z-index: 1000;
   }
-  .images{
-    position:absolute;
-    width:150px;
-    left:2px;
+  .images {
+    position: absolute;
+    width: 150px;
+    left: 2px;
 
-    img{
-        width:100%;
-        height:100%;
+    img {
+      width: 100%;
+      height: 100%;
     }
   }
 
-  .right-wrapper{
-    position:absolute;
-    width:150px;
+  .right-wrapper {
+    position: absolute;
+    width: 150px;
     right: -5%;
     bottom: -80px;
-    img{
-        width:100%;
-        height:100%;
+    img {
+      width: 100%;
+      height: 100%;
     }
-}
-.left-wrapper{
+  }
+  .left-wrapper {
     position: absolute;
     width: 150px;
     left: -2%;
     top: -80px;
-    img{
-        width:100%;
-        height:100%;
+    img {
+      width: 100%;
+      height: 100%;
     }
-}
+  }
 `;
