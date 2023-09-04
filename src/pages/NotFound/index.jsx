d9c6c5185ecb35ec0c6779ab60cbd8011a9a5eb8
hebamesluh1 <hebamesluh@gmail.com>
2023-09-04 23:39:03 +0300

    fixed warning

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import img from '../../assets/notFound.gif';
-import FullHeight from '../../components/FullHeight';
 import { Container } from '../../global/component';
 
 const NotFound = () => {
