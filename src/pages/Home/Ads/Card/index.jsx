380f739b1d7ce6eca7626d97b4bd3d4797c6776d
hebamesluh1 <hebamesluh@gmail.com>
2023-09-04 18:20:10 +0300

    ...

--- src/pages/Home/Ads/Card/index.jsx
+++ src/pages/Home/Ads/Card/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import { CardStyle } from './style';
 
 const Card = ({ icons, num, title }) => {
--- src/pages/Home/packages/Card/index.jsx
+++ src/pages/Home/packages/Card/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Wrapper } from './style';
-import BtnOrder from '../../../../components/BtnOrder';
-import { useTranslation } from 'react-i18next';
+
 import { motion } from 'framer-motion';
-import {fadeIn} from '../../../../utils/motion'
+import { fadeIn } from '../../../../utils/motion'
+
+import { Wrapper } from './style';
+
+import BtnOrder from '../../../../components/BtnOrder';
+
+import { useTranslation } from 'react-i18next';
+
+
 
 const Card = ({ img, title, desc, price, num = 1, options }) => {
   const { t } = useTranslation();
 
   return (
     <Wrapper>
-        <motion.span  variants={fadeIn("right", "tween", 0.2, 1)}>
+      <motion.span variants={fadeIn("right", "tween", 0.2, 1)}>
         <div className='image'>
           <img src={img} alt='' />
         </div>
@@ -32,8 +38,8 @@ const Card = ({ img, title, desc, price, num = 1, options }) => {
           <h4>{price}</h4>
           <BtnOrder />
         </div>
-    </motion.span>
-      </Wrapper>
+      </motion.span>
+    </Wrapper>
   );
 };
 
