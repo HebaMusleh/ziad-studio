3a928674334f302e71d22fe7ce3e71dbfad9f2cb
hebamesluh1 <hebamesluh@gmail.com>
2023-09-04 18:16:31 +0300

    ...

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Style = styled.div`
   label {
-    color: ${props=>props.theme.palette.labelColor};
+    color: ${(props) => props.theme.palette.labelColor};
     font-size: 1rem;
     font-weight: 500;
     line-height: 27px;
@@ -12,9 +12,9 @@ export const Style = styled.div`
     width: 100%;
     margin-top: 10px;
     margin-bottom: 25px;
-    background: ${props=>props.theme.palette.inputBG};
+    background: ${(props) => props.theme.palette.inputBG};
     border-radius: 8px;
-    border: 1px solid ${props=>props.theme.palette.inputBG};
+    border: 1px solid ${(props) => props.theme.palette.inputBG};
     padding: 15px;
     outline: none;
     font-size: 1rem;
@@ -25,13 +25,13 @@ export const Style = styled.div`
   textarea {
     width: 100%;
     margin-top: 20px;
-    background: ${props=>props.theme.palette.inputBG};
+    background: ${(props) => props.theme.palette.inputBG};
     border-radius: 8px;
-    border: 1px solid ${props=>props.theme.palette.inputBG};
+    border: 1px solid ${(props) => props.theme.palette.inputBG};
     resize: none;
     outline: none;
     font-size: 1rem;
-    padding:20px;
+    padding: 20px;
     &:focus {
       border: 1px solid #1f81b0;
     }
