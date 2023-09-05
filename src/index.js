42dda8c4b94c26e83d672ecb5a96060277341e97
hebamesluh1 <hebamesluh@gmail.com>
2023-09-05 11:58:35 +0300

    add error boundary

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './i18n/index.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./i18n/index.js";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
-
