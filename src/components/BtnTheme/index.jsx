1077efe716db3a97e1ef59bc16b4593149743f0a
hebamesluh1 <hebamesluh@gmail.com>
2023-09-05 20:43:18 +0300

    first edited done

--- src/components/BtnTheme/index.jsx
+++ src/components/BtnTheme/index.jsx
@@ -3,31 +3,22 @@ import React, { useContext } from 'react';
 import { themeContext } from '../../context/themeContext';
 import { darkTheme, lightTheme } from '../../global/theme';
 
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { Style } from './style';
 
-import './style.css'
-
-
-const BtnTheme = ({ dir }) => {
+const BtnTheme = () => {
   const [theme, setTheme] = useContext(themeContext);
-  const [isOn, setIsOn] = useState(false);
   const toggleTheme = () => {
     setTheme((theme) => (theme.theme === 'light' ? darkTheme : lightTheme));
     localStorage.setItem('theme', theme.theme === 'dark' ? 'light' : 'dark');
-    setIsOn(!isOn)
-  };
-  const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30
   };
   return (
-    <div className="theme-btn-wrapper">
-      <div className="switch" data-isOn={isOn} onClick={toggleTheme}>
-        <motion.div className="handle" layout transition={spring} />
-      </div>
-    </div>
+    <Style>
+      <label className="switch">
+        <input type="checkbox" onChange={toggleTheme} checked={theme.theme === 'dark'}/>
+        <span className="slider">
+        </span>
+      </label>
+    </Style>
   );
 }
 
