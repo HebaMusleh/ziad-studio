66b47fbbc7067d0e871b156d5e791da6eca22fd8
hebamesluh1 <hebamesluh@gmail.com>
2023-09-05 21:21:36 +0300

    .

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 
 export const HeaderStyle = styled.div`
-
-background: ${(props) => props.theme.palette.heroBackground};
-transition:.8s;
-position:fixed;
-width:100%;
-z-index:1;
-@media (max-width: 480px) {
-  position: relative;
-  padding:20px;
-}
+  background: ${(props) => props.theme.palette.heroBackground};
+  transition: 0.8s;
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+  @media (max-width: 480px) {
+    position: relative;
+  }
   .headers-own-style {
     display: flex;
     align-items: center;
@@ -22,14 +20,13 @@ z-index:1;
     cursor: pointer;
     color: ${(props) => props.theme.palette.primaryHeroColor};
   }
-
-  .logo{
+  .logo {
     @media (max-width: 480px) {
-      width:30px;
+      width: 30px;
     }
-    img{
-      width:100%;
-      height:100%;
+    img {
+      width: 100%;
+      height: 100%;
     }
   }
 `;
