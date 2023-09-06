481fdcff710de7c0ba0528dec5a908cc1c0297ce
hebamesluh1 <hebamesluh@gmail.com>
2023-09-06 10:50:09 +0300

    loadinf spinner done

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -1,43 +1,52 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../../assets/logo.png'
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { useTranslation } from 'react-i18next';
+
+const spinUpDown = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+`;
+
+const fade = keyframes`
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+`;
 
 const SpinnerStyle = styled.div`
-.splash-screen {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 20%;
-    height: 20%;
-    background-color: transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    animation: splashAnimation 2s ease-in-out forwards;
-    z-index: 10000000000000;
-  }
-  
-  @keyframes splashAnimation {
-    0% {
-      transform: translate(-50%, -50%) scale(0.1) rotate(0deg);
-      opacity: 0;
-    }
-  
-    100% {
-      transform: translate(-50%, -50%) scale(1) rotate(360deg);
-      opacity: 1;
-    }
-  }
-`
+  position: fixed;
+  top: 40%;
+  left: 40%;
+  margin:auto;
+  transform: translate(-50%, -50%);
+  width: 20%;
+  height: 20%;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10000000000000;
+  color: ${(props) => props.theme.palette.iconColor};
+  font-weight: bold;
+  font-size: 3rem;
+  text-align: center;
+  animation: ${spinUpDown} 2s linear infinite, ${fade} 2s ease-in-out infinite alternate;
+`;
+
 const Spinner = () => {
+  const { t } = useTranslation();
   return (
     <SpinnerStyle>
-      <div className="splash-screen">
-        <img src={logo} alt='logo' loading='lazy' />
-      </div>
-    </SpinnerStyle >
-  )
-}
+      <div>{t('z')}</div>
+    </SpinnerStyle>
+  );
+};
 
-export default Spinner
+export default Spinner;
