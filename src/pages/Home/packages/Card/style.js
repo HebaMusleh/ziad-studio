61a00caba979cf1c6d2d34ce0f61734a4673d454
hebamesluh1 <hebamesluh@gmail.com>
2023-09-06 10:26:46 +0300

    .

--- src/pages/Home/packages/Card/style.js
+++ src/pages/Home/packages/Card/style.js
@@ -2,21 +2,27 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   background: ${(props) => props.theme.palette.packagesBackground};
-  box-shadow: 0px 4px 25px 0px #82C8DC66;
+  box-shadow: 0px 4px 25px 0px #82c8dc66;
   border-radius: 48px;
   flex-basis: 30%;
   padding: 40px;
   height: fit-content;
   .image {
     background: ${(props) => props.theme.palette.whiteFonts};
-    width: 160px;
-    height: 160px;
+    width: 80px;
     border-radius: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
     margin: auto;
     margin-bottom: 30px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    @media (max-width: 480px) {
+      width: 50px;
+    }
   }
   .title {
     h3 {
@@ -26,12 +32,12 @@ export const Wrapper = styled.div`
       letter-spacing: 0em;
       color: ${(props) => props.theme.palette.TitleColor};
       margin-bottom: 20px;
-      text-align:center;
+      text-align: center;
       @media (max-width: 768px) {
         font-size: 27px;
       }
       @media (max-width: 480px) {
-        font-size:20px;
+        font-size: 20px;
       }
     }
   }
@@ -61,14 +67,14 @@ export const Wrapper = styled.div`
       letter-spacing: 0em;
       color: ${(props) => props.theme.palette.priceColor};
       margin: 30px 0;
-      text-align:center;
+      text-align: center;
       @media (max-width: 768px) {
         font-size: 1rem;
       }
     }
   }
   ul {
-    padding:40px;
+    padding: 40px;
     li {
       font-size: 1rem;
       font-weight: 500;
@@ -78,16 +84,16 @@ export const Wrapper = styled.div`
       color: #636363;
       text-align: start;
       @media (max-width: 768px) {
-        font-size: .9rem;
+        font-size: 0.9rem;
       }
     }
-    .bold{
-      font-weight:bold;
-      color:${(props) => props.theme.palette.bold};
+    .bold {
+      font-weight: bold;
+      color: ${(props) => props.theme.palette.bold};
     }
-    .extraBold{
-      font-weight:bold;
-      color:${(props) => props.theme.palette.extrabold};
+    .extraBold {
+      font-weight: bold;
+      color: ${(props) => props.theme.palette.extrabold};
     }
   }
   .edits {
@@ -96,12 +102,12 @@ export const Wrapper = styled.div`
     font-size: 1.5rem;
     font-weight: 500;
     line-height: 41.09px;
-    text-align:center;
+    text-align: center;
     @media (max-width: 768px) {
       font-size: 1rem;
     }
   }
-  .brand{
+  .brand {
     color: ${(props) => props.theme.palette.icon6Bg};
   }
 `;
