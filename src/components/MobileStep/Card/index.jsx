6185756c314857eccf6e0baca42c98cd0d678fc5
hebamesluh1 <hebamesluh@gmail.com>
2023-09-07 09:16:03 +0300

    ...

--- src/components/MobileStep/Card/index.jsx
+++ src/components/MobileStep/Card/index.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 import { Style } from './style';
 
-const Card = ({icon,desc,bg}) => {
+const Card = ({ icon, desc, bg, id }) => {
     return (
         <Style bg={bg}>
             <div className="wrapper-icons">
-            <div className="icons">
-                <img src={icon} alt="" />
-            </div>
+                <div className="icons">
+                    <img src={icon} alt="" />
+                </div>
             </div>
             <div className="description">
-                <p>{desc}</p>
+                <p> {id} -
+                    {desc}</p>
             </div>
         </Style>
     )
--- src/components/MobileStep/index.jsx
+++ src/components/MobileStep/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Card from './Card'
-import { PiArrowArcRightFill, PiArrowArcLeftFill } from 'react-icons/pi'
-import styled from 'styled-components'
-import Data from '../../mock/data'
+import React from 'react';
+import Card from './Card';
+import { PiArrowArcRightFill } from 'react-icons/pi';
+import styled from 'styled-components';
+import Data from '../../mock/data';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,31 +10,31 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
   margin-top: 30px;
-  direction:ltr;
+  direction: ltr;
   .arrow {
     font-weight: 500;
     font-size: 30px;
-    color:${props=>props.theme.palette.secondaryCustomerColor};
+    color: ${props => props.theme.palette.secondaryCustomerColor};
   }
-`
+`;
 
 const MobileStep = () => {
-  const data = Data();
+  const sortedData = Data().sort((a, b) => a.id - b.id);
 
   return (
     <Wrapper>
-      {data.map((item) => (
+      {sortedData.map((item) => (
         <React.Fragment key={item.id}>
-          <Card icon={item.icon} desc={item.title} bg={item.iconBg} />
-          {item.id <4 ? (
-            <PiArrowArcRightFill className='arrow' />
+          <Card icon={item.icon} desc={item.title} bg={item.iconBg} id={item.id} />
+          {item.id === 3 || item.id === 6 ? (
+            ""
           ) : (
-            <PiArrowArcLeftFill className='arrow' />
+            <PiArrowArcRightFill className='arrow' />
           )}
         </React.Fragment>
       ))}
     </Wrapper>
-  )
-}
+  );
+};
 
-export default MobileStep
+export default MobileStep;
